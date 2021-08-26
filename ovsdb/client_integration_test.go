973dde8af0424acb7d784454263f07c1239cc071
Franck Rupin <frupin@digitalocean.com>
2021-08-26 06:30:50 -0400

    Try a fix

--- ovsdb/client_integration_test.go
+++ ovsdb/client_integration_test.go
@@ -89,7 +89,7 @@ func testClientDatabases(ctx context.Context, t *testing.T, c *ovsdb.Client) {
 	}
 	t.Logf("----- GET DBS: %v", dbs)
 
-	want := []string{"Open_vSwitch"}
+	want := []string{"Open_vSwitch", "_Server"}
 
 	if diff := cmp.Diff(want, dbs); diff != "" {
 		t.Fatalf("unexpected databases (-want +got):\n%s", diff)
